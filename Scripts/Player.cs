057959e0d26265557172d5d21e1f932c5f9b377a
eri-ro <er272456@ucf.edu>
2025-11-07 21:42:31 -0500

    cleaning up

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -4,12 +4,6 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    //how to define a variable
-    //1. access modifier: public or private
-    //2. data type: int, float, bool, string
-    //3. variable name: camelCase
-    //4. value: optional
-
     private float playerSpeed;
     private float horizontalInput;
     private float verticalInput;
@@ -32,18 +26,17 @@ public class Player : MonoBehaviour
         //This function is called every frame; 60 frames/second
         Movement();
         Shooting();
-
     }
 
     void Shooting()
     {
         //if the player presses the SPACE key, create a projectile
-        if (Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(bulletPrefab, transform.position + new Vector3(0, 1, 0), Quaternion.identity);
         }
-        
-        //if player presses E key, create a Big Bullet
+
+         //if player presses E key, create a Big Bullet
         if (Input.GetKeyDown(KeyCode.E))
         {
             Instantiate(bigBulletPrefab, transform.position + new Vector3(0, 2, 0), Quaternion.identity);
@@ -57,23 +50,20 @@ public class Player : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
         //Move the player
         transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * Time.deltaTime * playerSpeed);
-
-        //Player limited to bottom half of the screen
-        if (transform.position.x > 0
+        //Player leaves the screen horizontally
+        if(transform.position.x > horizontalScreenLimit || transform.position.x <= -horizontalScreenLimit)
         {
-            transform.position = new Vector3(0, transform.position.y, 0);
+            transform.position = new Vector3(transform.position.x * -1, transform.position.y, 0);
         }
-
-        if (transform.position.x < -horizontalScreenLimit)
+        //Player is limited to the bottom half of the screen
+        if(transform.position.y > 0)
         {
-            transform.position = new Vector3(-horizontalScreenLimit, transform.position.y, 0);
+            transform.position = new Vector3(transform.position.x, 0, 0);
         }
         
-        //Player leaves the screen vertically
-        if(transform.position.y > verticalScreenLimit || transform.position.y < -verticalScreenLimit)
+        if(transform.position.y < -verticalScreenLimit)
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y * -1, 0);
+            transform.position = new Vector3(transform.position.x, -verticalScreenLimit, 0);
         }
     }
-
 }
