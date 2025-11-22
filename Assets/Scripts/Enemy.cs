5aca6259fa7c26832d4eacfe7939de6fee93f7ed
Dave <gigantamaxed@gmail.com>
2025-11-22 16:20:33 -0500

    enemy1 movement and fonts
    changed enemy1 movement and added new fonts

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,17 +8,27 @@ public class Enemy : MonoBehaviour
     public GameObject explosionPrefab;
     
     private GameManager gameManager;
+     private float startX;
 
     // Start is called before the first frame update
     void Start()
     {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+         startX = transform.position.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(new Vector3(0, -2, 0) * Time.deltaTime * 0.5f);
+
+	float newX = startX + Mathf.Sin(Time.time * 5f) * 1f;
+  	transform.position = new Vector3(newX, transform.position.y, transform.position.z);   
+
+        if (transform.position.y < -5.5f)
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D whatDidIHit)
